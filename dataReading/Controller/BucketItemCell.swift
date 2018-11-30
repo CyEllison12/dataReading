900c8f417b58e0f95a64c01266360840bca214d8
Cy Ellison <dragonz2626@gmail.com>
2018-11-30 11:45:50 -0700

    added all the files needed

---
+++ dataReading/Controller/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItemCell.swift
+//  dataReading
+//
+//  Created by Ellison, Cy on 11/30/18.
+//  Copyright © 2018 Ellison, Cy. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ dataReading/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  dataReading
+//
+//  Created by Ellison, Cy on 11/30/18.
+//  Copyright © 2018 Ellison, Cy. All rights reserved.
+//
+
+import Foundation
